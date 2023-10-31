4ad07ed5686e7d8a845552c2ca2beddcca67e067
James Nichols <jnichols.tigley@gmail.com>
2023-10-31 10:27:48 +0000

    Map rendering works.

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
 import random
 import math
 
-MAP_WIDTH = 200
-MAP_HEIGHT = 200
+MAP_WIDTH = 50
+MAP_HEIGHT = 50
 
-MAP_SAMPLE_SEGMENTS = 100
+MAP_SAMPLE_SEGMENTS = 360
 POINT_SHIFT_SAMPLES = 100
-POINT_SHIFT_MAX_DISTANCE = 5
+POINT_SHIFT_MAX_DISTANCE = 10
 
 def setupMapDictionary():
 
     map = {}
 
-    for y in range(0, MAP_HEIGHT):
-        for x in range(0, MAP_WIDTH):
+    for y in range(MAP_HEIGHT):
+        for x in range(MAP_WIDTH):
             map[(x, y)] = " "
     
     return map
 
 def generateMap(state):
 
-    emptyMap = setupMapDictionary()
+    map = setupMapDictionary()
 
     sampleStep = (2 * math.pi) / MAP_SAMPLE_SEGMENTS
 
@@ -28,7 +28,7 @@ def generateMap(state):
 
     radius = (MAP_WIDTH - centreX) * 0.8
 
-    for i in range(0, MAP_SAMPLE_SEGMENTS):
+    for i in range(MAP_SAMPLE_SEGMENTS):
 
         #Get point on circle at i
         
@@ -40,26 +40,42 @@ def generateMap(state):
 
         #Offset each point a bit
 
-        xChangePerSample, yChangePerSample = (circleX - centreX), (circleY - centreY)
+        xChangePerSample, yChangePerSample = (circleX - centreX) / POINT_SHIFT_SAMPLES, (circleY - centreY) / POINT_SHIFT_SAMPLES
 
         pointShiftAmount = (random.random()*2)-1
 
         sampleNo = pointShiftAmount * POINT_SHIFT_MAX_DISTANCE
 
-        actualX, actualY = math.floor(xChangePerSample * sampleNo), math.floor(yChangePerSample * sampleNo)
+        actualX, actualY = math.floor(circleX + xChangePerSample * sampleNo), math.floor(circleY + yChangePerSample * sampleNo)
 
-        emptyMap[(actualX, actualY)] = "X"
+        map[(actualX, actualY)] = "X"
 
+    state["mapData"] = map
 
+def renderMap(state):
+    mapData = state["mapData"]
 
+    mapLines = []
+
+    for y in range(MAP_HEIGHT):
+        mapLine = []
+        for x in range(MAP_WIDTH):
+            mapLine.append(mapData[(x, y)])
         
-
+        mapLines.append(" ".join(mapLine)+"\n")
+    
+    with open("test.txt","w") as f:
+        f.writelines(mapLines)
 
 
 def generateState():
     state = {"playerInfo":{"position":{"x":0,"y":0}},"mapData":{}, "objectData":{}}
 
+    generateMap(state)
+
+    renderMap(state)
+
     return state
 
 if __name__ == "__main__":
-    generateMap({})
+    generateState()
